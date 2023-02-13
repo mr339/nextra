c9eebd6574a5bd2dd6cfbc32e75c76443ff19b11
Ganesh Raj Manandhar <ek_mac_46@Ganesh339.local>
2023-02-13 09:45:58 +0545

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx',
+    staticImage: true,
+    latex: true,
+    flexsearch: {
+        codeblock: false
+    }
+})
+
+module.exports = withNextra({ reactStrictMode: true })
+
+
---
+++ theme.config.jsx
@@ -0,0 +1,6 @@
+export default {
+    logo: <span>CMS codebase documentation</span>,
+    project: {
+        link: 'https://github.com/shuding/nextra',
+    },
+}
