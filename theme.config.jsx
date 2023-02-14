7d5ab4d1634f89701bd228ce290a37eebdbaa683
Ganesh Raj Manandhar <ek_mac_46@Ganesh339.local>
2023-02-14 11:35:10 +0545

    fix: disabled links on right nav bar and removed security headers for now

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,12 @@
 export default {
     logo: <span>CMS codebase documentation</span>,
     project: {
-        link: 'https://github.com/shuding/nextra',
+        link: 'https://repo.ekbana.info/frontend-v2/codebase-nextra-documentation',
     },
+    feedback: {
+        content: null //Disable the feedback lin on rightNavBar
+    },
+    editLink: {
+        text: '' //Hide the editLink on rightNavBar
+    }
 }
