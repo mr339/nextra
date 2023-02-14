e6001b1c30002005dfe515bd6a4b948d59f20890
Ganesh Raj Manandhar <ek_mac_46@Ganesh339.local>
2023-02-14 14:54:25 +0545

    feat: added strict transport policy header

--- next.config.js
+++ next.config.js
@@ -33,6 +33,10 @@ module.exports = withNextra({
                     {
                         key: 'Content-Security-Policy',
                         value: `object-src 'none'; frame-ancestors 'none'` //The object-src directive is set to 'none' to prevent plugins from being loaded.
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload'
                     }
                 ]
             }
